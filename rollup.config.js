f8d5d1f2f1c4d7fe53c5fbf0d7824efc9e6411d7
Simon Garner <simon@equalogic.com>
2025-08-04 11:47:26 +1200

    Use rollup preserveModules mode

--- rollup.config.js
+++ rollup.config.js
@@ -4,12 +4,17 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 
 const config = {
-  input: ['src/action.ts', 'src/cli.ts'],
+  input: {
+    cli: 'src/cli.ts',
+    action: 'src/action.ts',
+  },
   output: {
     dir: 'dist',
     format: 'es',
     esModule: true,
     sourcemap: true,
+    preserveModules: true,
+    preserveModulesRoot: 'src',
   },
   plugins: [commonjs(), json(), nodeResolve({ preferBuiltins: true }), typescript()],
 };
