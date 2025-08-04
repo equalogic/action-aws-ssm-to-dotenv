ac475b4c47d66154738bc7a3b73fbe25f4f4c9e4
Simon Garner <simon@equalogic.com>
2025-08-04 12:18:54 +1200

    Don’t require ‘format’ and ‘output’ action inputs

--- src/action.ts
+++ src/action.ts
@@ -6,8 +6,8 @@ async function run() {
   try {
     const region = process.env.AWS_DEFAULT_REGION;
     const ssmPath = core.getInput('ssm-path', { required: true });
-    const format = core.getInput('format', { required: true });
-    const output = core.getInput('output', { required: true });
+    const format = core.getInput('format') || 'github-actions';
+    const output = core.getInput('output') || '.env';
     const prefix = core.getInput('prefix');
     const withDecryption = core.getInput('decryption') === 'true';
 
